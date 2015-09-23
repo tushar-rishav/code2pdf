256b197cf84b0551f912c0724656db6b854dd1da
Christopher Welborn <cj@welbornprod.com>
2015-09-23 14:25:19 -0500

    Support for multiple languages, line numbers.
    With pygments, several source file types are supported.
    Html is formatted with optional line  numbers before converting
    to PDF.
    
    Output file name defaults to current directory and
    inputfile.pdf (replacing the old extension)

--- Py2pdf/py2pdf.py
+++ Py2pdf/py2pdf.py
@@ -1,11 +1,19 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 from PyQt4.QtGui import QTextDocument, QPrinter, QApplication
 import os
 import sys
 import logging
 import argparse
-import tempfile
-import subprocess
+
+try:
+    import pygments
+    from pygments import lexers, formatters, styles
+except ImportError as ex:
+    print('\nCould not import the required "pygments" module:\n{}'.format(
+        ex))
+    sys.exit(1)
+
+__version__ = '0.2.0'
 
 
 def logger(func):
@@ -16,42 +24,65 @@ def logger(func):
     return log_wrap
 
 
-class Py2pdf:
+class Source2pdf:
 
     """
-            converts python file to pdf
+            Convert a source file into a pdf with syntax highlighting.
     """
     @logger
     def __init__(self, ifile=None, ofile=None, size="A4"):
         self.size = size
         if not ifile:
             raise Exception("input file is required")
-        else:
-            self.py_file = ifile
-            if ofile:
-                self.pdf_file = ofile
-            else:
-                self.pdf_file = ifile.split('.')[0]+".pdf"
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as temp:
-                self.temp_file = temp.name
-                try:
-                    temp_data = open(self.py_file, 'rb').read()
-                    temp.write(temp_data)
-                    temp.flush()
-                except Exception as e:
-                    print(e)
-                    sys.exit(1)
 
-    def init_print(self):
-        app = QApplication(sys.argv)
+        self.input_file = ifile
+        if ofile:
+            self.pdf_file = ofile
+        else:
+            self.pdf_file = ifile.split('.')[0] + ".pdf"
+
+    def highlight_file(self, linenos=True, style='default'):
+        """ Highlight the input file, and return HTML as a string. """
+        try:
+            lexer = lexers.get_lexer_for_filename(self.input_file)
+        except pygments.util.ClassNotFound:
+            # Try guessing the lexer (file type) later.
+            lexer = None
+
+        try:
+            formatter = formatters.HtmlFormatter(
+                linenos=linenos,
+                style=style,
+                full=True)
+        except pygments.util.ClassNotFound:
+            print("\nInvalid style name: {}\nExpecting one of:\n    {}".format(
+                style,
+                "\n    ".join(sorted(styles.STYLE_MAP))))
+            sys.exit(1)
+
+        try:
+            with open(self.input_file, "r") as f:
+                content = f.read()
+                if lexer is None:
+                    try:
+                        lexer = lexers.guess_lexer(content)
+                    except pygments.util.ClassNotFound:
+                        # No lexer could be guessed.
+                        lexer = lexers.get_lexer_by_name("text")
+        except EnvironmentError as exread:
+            print("\nUnable to read file: {}\n{}".format(
+                self.input_file,
+                exread))
+            sys.exit(1)
+
+        return pygments.highlight(content, lexer, formatter)
+
+    def init_print(self, linenos=True, style="default"):
+        app = QApplication(sys.argv)  # noqa
         doc = QTextDocument()
-        temp_html = self.temp_file[:-2] + "html"
-        r = "pygmentize -O full,style=emacs -f html -l python -o %s %s" % (
-            temp_html, self.py_file)
-        s = subprocess.Popen(r, shell=True)
-        s.wait()
-        html = open(temp_html).read()
-        doc.setHtml(html)
+        doc.setHtml(
+            self.highlight_file(linenos=linenos, style=style)
+        )
         printer = QPrinter()
         printer.setOutputFileName(self.pdf_file)
         printer.setOutputFormat(QPrinter.PdfFormat)
@@ -67,34 +98,67 @@ class Py2pdf:
         logging.info("PDF created at %s" % (self.pdf_file))
 
 
+def get_output_file(inputname, outputname=None):
+    """ If the output name is set, then return it.
+        Otherwise, build an output name using the current directory,
+        replacing the input name's extension.
+    """
+    if outputname:
+        return outputname
+
+    inputbase = os.path.split(inputname)[-1]
+    outputbase = "{}.pdf".format(os.path.splitext(inputbase)[0])
+    return os.path.join(os.getcwd(), outputbase)
+
 
 def parse_arg():
-    parser = argparse.ArgumentParser(description=" \
-             Convert Python code into pdf with syntax highlighting", epilog="\
-             Author:tushar.rishav@gmail.com")
+    parser = argparse.ArgumentParser(
+        description=(
+            "Convert Python code into pdf with syntax highlighting"),
+        epilog="Author:tushar.rishav@gmail.com"
+    )
     parser.add_argument(
-        "-i", "--ifile", help="Absolute path of the python file", type=str)
+        "filename",
+        help="absolute path of the python file",
+        type=str)
     parser.add_argument(
-        "-o", "--ofile", help="Absolute path of the output pdf file", type=str)
+        "-l",
+        "--linenos",
+        help="include line numbers.",
+        action="store_true")
     parser.add_argument(
-        "-s", "--size", help="PDF size. A2,A3,A4,A5 etc", type=str, default="A3")
-    args = parser.parse_args()
-    if not args.ifile:
-        raise Exception("input file is required")
-    else:
-        ifile = args.ifile
-        if args.ofile:
-            pdf_file = args.ofile
-        else:
-            pdf_file = args.ifile.split('.')[0]+".pdf"
-    return (ifile, pdf_file, args.size)
+        "outputfile",
+        help="absolute path of the output pdf file",
+        nargs="?",
+        type=str)
+    parser.add_argument(
+        "-s",
+        "--size",
+        help="PDF size. A2,A3,A4,A5 etc",
+        type=str,
+        default="A3")
+    parser.add_argument(
+        "-S",
+        "--style",
+        help="the style name for highlighting.",
+        type=str,
+        default="default",
+        metavar="NAME")
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version="%(prog)s v. {}".format(__version__))
+    return parser.parse_args()
 
 
 def main():
 
-    ifile, ofile, size = parse_arg()
-    pdf = Py2pdf(ifile, ofile, size)
-    pdf.init_print()
+    args = parse_arg()
+    pdf_file = get_output_file(args.filename, args.outputfile)
+    pdf = Source2pdf(args.filename, pdf_file, args.size)
+    pdf.init_print(linenos=args.linenos, style=args.style)
+    return 0
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
