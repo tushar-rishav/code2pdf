010b6a4999b337677c7195cd847fe0dd0230a68f
Tushar Gautam <tushar.rishav@gmail.com>
2015-09-30 16:36:10 +0530

    more test cases

--- tests/test.py
+++ tests/test.py
@@ -1,21 +1,42 @@
-import subprocess
+import os
+import Code2pdf
 import unittest2
+from Code2pdf.code2pdf import *
+try:
+    import pygments
+    from pygments import lexers, formatters, styles
+except ImportError as ex:
+    logging.warning('\nCould not import the required \
+        "pygments" module:\n{}'.format(
+        ex))
+    sys.exit(1)
 
 
 class Code2pdfTestCase(unittest2.TestCase):
-    """Tests for `balert/main.py`."""
-    def setUp(self):
-        pass
-    def run_code2pdf(self):
-        try:
-            py2pdf_run = subprocess.Popen("code2pdf -h \
-                ", shell=True, stdout=subprocess.PIPE).stdout.read()
-            return True
-        except:
-            return False
 
-    def test_code2pdf(self):
-        self.assertTrue(self.run_code2pdf())
+    """Tests for `balert/main.py`."""
+
+    def setUp(self):
+        self.filename = "test.py"
+        self.pdf_file = os.path.abspath("test.pdf")
+        self.size = "a4"
+        self.style = "emacs"
+        self.linenos = True
+        self.output_file = None
+
+    def test_init_print(self):
+        pdf = Code2pdf(self.filename, self.pdf_file, self.size)
+        pdf.init_print(self.linenos, self.style)
+        self.assertTrue(os.path.exists(pdf.pdf_file))    # if pdf printed
+
+    def test_highlight_file(self):
+        hpdf = Code2pdf(self.filename, self.pdf_file, self.size)
+        html = hpdf.highlight_file(True, 'emacs')
+        self.assertIn("!DOCTYPE", html)                 # if html created
+
+    def test_get_output_file(self):
+        self.assertEqual(
+            get_output_file(self.filename, self.output_file), self.pdf_file)
 
 if __name__ == "__main__":
     unittest2.main()
