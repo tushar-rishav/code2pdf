6409b06ca635d6cff5257ae2108ea304e585c60f
Tushar Gautam <tushar.rishav@gmail.com>
2015-10-23 23:35:58 +0530

    Uncle Sam

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/sh
+
+wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.16.9/sip-4.16.9.tar.gz
+tar -zxvf sip-4.16.9.tar.gz
+cd sip-4.16.9/
+python configure.py
+make
+make install
